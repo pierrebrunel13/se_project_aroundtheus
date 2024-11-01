54a4379d24c9f4008746ba662cc3c95cc041d77a
Pierrebrunel <pierrebruneljoseph13@gmail.com>
2024-11-01 15:27:11 -0400

    part 3

--- scripts/index.js
+++ scripts/index.js
@@ -38,5 +38,18 @@ const initialCards = [
 
 ];
 
-
+/*Element*/
+ 
 console.log(initialCards);
+
+const profileEditButton = document.querySelector("#profile__edit-button");
+const profileEditModal = document.querySelector('#profile-edit-modal');
+const modalCloseButton = document.querySelector(".modal__close");
+
+profileEditButton.addEventListener('click', () => {
+   profileEditModal.classList.add("modal__opened")
+});
+
+modalCloseButton.addEventListener('click', () => {
+profileEditModal.classList.remove("modal__opened");
+} ); 
