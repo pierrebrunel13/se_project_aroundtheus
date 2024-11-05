f25c3e8a19f42c0c6d4fda92c6994e38c7c2d634
Pierrebrunel <pierrebruneljoseph13@gmail.com>
2024-11-05 04:15:50 -0500

    set up

--- scripts/index.js
+++ scripts/index.js
@@ -45,11 +45,34 @@ console.log(initialCards);
 const profileEditButton = document.querySelector("#profile__edit-button");
 const profileEditModal = document.querySelector('#profile-edit-modal');
 const modalCloseButton = document.querySelector(".modal__close");
+const profileTitle = document.querySelector("#profile__title");
+const profileDescription = document.querySelector("#profile__description");
+const profileTitleInput = document.querySelector("#profile-title-input");
+const profileDescriptionInput = document.querySelector("#profile-description-input");
+const profileEditForm = profileEditModal.querySelector(".modal__form");
+
+/*functions*/
+
+function closePopop() {
+    profileEditModal.classList.remove("modal__opened");
+}
+
+/* EVENT LISTENER */
 
 profileEditButton.addEventListener('click', () => {
+profileTitleInput.value = profileTitle.textContent;
+profileDescriptionInput.value = profileDescription.textContent;
+
    profileEditModal.classList.add("modal__opened")
 });
 
 modalCloseButton.addEventListener('click', () => {
-profileEditModal.classList.remove("modal__opened");
+closePopop();
 } ); 
+
+profileEditForm.addEventListener('submit', (e) => {
+e.preventDefault();
+profileTitle.textContent = profileTitleInput.value;
+profileDescription.textContent = profileDescriptionInput.value;
+closePopop();
+});
