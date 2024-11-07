c650ed4241b8df0a4b835be563b2c2b7ec8a7f11
Pierrebrunel <pierrebruneljoseph13@gmail.com>
2024-11-06 22:48:09 -0500

    almost done

--- scripts/index.js
+++ scripts/index.js
@@ -13,13 +13,11 @@ const initialCards = [
 {
     name: "Bald Mountains",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg"
-
 },
 
 {
     name: "Latemar",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg"
-
 
 },
 
@@ -27,20 +25,17 @@ const initialCards = [
     name: "Vanoise National Park",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg"
 
-
 },
 
 {
     name: "Lago di Braies",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg"
-
 },
 
 ];
 
 /*Element*/
- 
-console.log(initialCards);
+  
 
 const profileEditButton = document.querySelector("#profile__edit-button");
 const profileEditModal = document.querySelector('#profile-edit-modal');
@@ -50,11 +45,28 @@ const profileDescription = document.querySelector("#profile__description");
 const profileTitleInput = document.querySelector("#profile-title-input");
 const profileDescriptionInput = document.querySelector("#profile-description-input");
 const profileEditForm = profileEditModal.querySelector(".modal__form");
+const cardListEl = document.querySelector(".cards__list");
+const cardTemplate = document.querySelector("#card-template").content.firstElementChild;
 
 /*functions*/
 
 function closePopop() {
     profileEditModal.classList.remove("modal__opened");
+}
+
+function getCardElement(cardData) {
+ //clone the template element with all its content and store it in a cardElement variable
+ const cardElement = cardTemplate.cloneNode(true);
+ console.log(cardData.name);
+ //access the card title and image and store them in variables
+ const cardImageEl = cardElement.querySelector(".card__image");
+ const cardTitleEl = cardElement.querySelector(".card__title");
+ //set the path to the image to the link field of the object
+ //set the image alt text to the name field of the object
+ //set the card title to the name field of the object, too
+ cardTitleEl.textContent = cardData.name;
+ //return the ready HTML element with the filled-in data
+ return cardElement;
 }
 
 /* Events handlers */
@@ -77,3 +89,8 @@ profileDescriptionInput.value = profileDescription.textContent;
 modalCloseButton.addEventListener('click', closePopop); 
 
 profileEditForm.addEventListener('submit',handleProfileEditSubmit);
+
+initialCards.forEach((cardData) => {
+   const cardElement = getCardElement(cardData);
+    cardListEl.prepend(cardElement);
+});
