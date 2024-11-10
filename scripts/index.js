63b15fc47e69ab2294d67a149f6a1f843635d04d
Pierrebrunel <pierrebruneljoseph13@gmail.com>
2024-11-10 10:35:55 -0500

    prettierignore

--- scripts/index.js
+++ scripts/index.js
@@ -1,97 +1,95 @@
-
 const initialCards = [
-{
+  {
     name: "Yosemite Valley",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg"
-},
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
+  },
 
-{
+  {
     name: "Lake Louise",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg"
-},
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg",
+  },
 
-{
+  {
     name: "Bald Mountains",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg"
-},
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg",
+  },
 
-{
+  {
     name: "Latemar",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg"
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg",
+  },
 
-},
-
-{
+  {
     name: "Vanoise National Park",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg"
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg",
+  },
 
-},
-
-{
+  {
     name: "Lago di Braies",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg"
-},
-
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
+  },
 ];
 
 /*Element*/
-  
 
 const profileEditButton = document.querySelector("#profile__edit-button");
-const profileEditModal = document.querySelector('#profile-edit-modal');
+const profileEditModal = document.querySelector("#profile-edit-modal");
 const modalCloseButton = document.querySelector(".modal__close");
 const profileTitle = document.querySelector("#profile__title");
 const profileDescription = document.querySelector("#profile__description");
 const profileTitleInput = document.querySelector("#profile-title-input");
-const profileDescriptionInput = document.querySelector("#profile-description-input");
+const profileDescriptionInput = document.querySelector(
+  "#profile-description-input"
+);
 const profileEditForm = profileEditModal.querySelector(".modal__form");
 const cardListEl = document.querySelector(".cards__list");
-const cardTemplate = document.querySelector("#card-template").content.firstElementChild;
+const cardTemplate =
+  document.querySelector("#card-template").content.firstElementChild;
 
 /*functions*/
 
 function closePopop() {
-    profileEditModal.classList.remove("modal__opened");
+  profileEditModal.classList.remove("modal__opened");
 }
 
 function getCardElement(cardData) {
- //clone the template element with all its content and store it in a cardElement variable
- const cardElement = cardTemplate.cloneNode(true);
- const cardImageEl = cardElement.querySelector(".card__image");
- //access the card title and image and store them in variables
- const cardTitleEl = cardElement.querySelector(".card__title");
- //set the path to the image to the link field of the 
- cardImageEl.src = cardData.link;
- cardImageEl.alt = cardData.name;
- //set the image alt text to the name field of the object
- //set the card title to the name field of the object, too
- cardTitleEl.textContent = cardData.name;
- //return the ready HTML element with the filled-in data
- return cardElement;
+  //clone the template element with all its content and store it in a cardElement variable
+  const cardElement = cardTemplate.cloneNode(true);
+  const cardImageEl = cardElement.querySelector(".card__image");
+  //access the card title and image and store them in variables
+  const cardTitleEl = cardElement.querySelector(".card__title");
+  //set the path to the image to the link field of the
+  cardImageEl.src = cardData.link;
+  cardImageEl.alt = cardData.name;
+  //set the image alt text to the name field of the object
+  //set the card title to the name field of the object, too
+  cardTitleEl.textContent = cardData.name;
+  //return the ready HTML element with the filled-in data
+  return cardElement;
 }
 
 /* Events handlers */
-function handleProfileEditSubmit(e)  {
-    e.preventDefault();
-    profileTitle.textContent = profileTitleInput.value;
-    profileDescription.textContent = profileDescriptionInput.value;
-    closePopop()};
-
+function handleProfileEditSubmit(e) {
+  e.preventDefault();
+  profileTitle.textContent = profileTitleInput.value;
+  profileDescription.textContent = profileDescriptionInput.value;
+  closePopop();
+}
 
 /* EVENT LISTENER */
 
-profileEditButton.addEventListener('click', () => {
-profileTitleInput.value = profileTitle.textContent;
-profileDescriptionInput.value = profileDescription.textContent;
+profileEditButton.addEventListener("click", () => {
+  profileTitleInput.value = profileTitle.textContent;
+  profileDescriptionInput.value = profileDescription.textContent;
 
-   profileEditModal.classList.add("modal__opened")
+  profileEditModal.classList.add("modal__opened");
 });
 
-modalCloseButton.addEventListener('click', closePopop); 
+modalCloseButton.addEventListener("click", closePopop);
 
-profileEditForm.addEventListener('submit',handleProfileEditSubmit);
+profileEditForm.addEventListener("submit", handleProfileEditSubmit);
 
 initialCards.forEach((cardData) => {
-   const cardElement = getCardElement(cardData);
-    cardListEl.prepend(cardElement);
+  const cardElement = getCardElement(cardData);
+  cardListEl.prepend(cardElement);
 });
