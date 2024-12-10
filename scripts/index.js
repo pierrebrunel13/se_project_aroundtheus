97264314ac31e4966f4a223eaa0103822a58e691
Pierrebrunel <pierrebruneljoseph13@gmail.com>
2024-12-10 04:43:41 -0500

    added the trash image

--- scripts/index.js
+++ scripts/index.js
@@ -70,7 +70,11 @@ function getCardElement(cardData) {
   const cardElement = cardTemplate.cloneNode(true);
   const cardImageEl = cardElement.querySelector(".card__image");
   const cardTitleEl = cardElement.querySelector(".card__title");
-  const likeButton = cardElement.querySelector();
+  const likeButton = cardElement.querySelector(".card__like-button");
+
+  likeButton.addEventListener("click",() => {
+    likeButton.classList.toggle("card__like-button_active");})
+
   cardImageEl.src = cardData.link;
   cardImageEl.alt = cardData.name;
   cardTitleEl.textContent = cardData.name;
@@ -105,10 +109,5 @@ initialCards.forEach((cardData) => {
 // add new card button
 addNewCardButton.addEventListener("click", () => openModal(profileAddModal));
 
-// const likeButtons = document.querySelectorAll(".card__like-button");
-// likeButtons.forEach((likeButton) => {
-  // likeButton.addEventListener("click",() => {
-// likeButton.classList.toggle("card__like-button_active");
-  // })
-// })
+
 
