9bdfec0b8467032eafa1e576b35484a4c2d103c4
Pierrebrunel <pierrebruneljoseph13@gmail.com>
2025-01-16 23:08:09 -0500

    g

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,12 +1,18 @@
-//console.log('hello from validation.js');
+
 // enabling validation by calling enableValidation()
 // pass all the settings on call
+function enableValidation(options) {
+    const formEls = [...document.querySelectorAll(".popup__form")];
+    console.log(formEls);
+};
 
-enableValidation({
+const config = {
     formSelector: ".popup__form",
-    inputSelector: ".popup__input",
+    inputSelector: ".popup__input", 
     submitButtonSelector: ".popup__button",
     inactiveButtonClass: "popup__button_disabled",
     inputErrorClass: "popup__input_type_error",
     errorClass: "popup__error_visible"
-  });
+  };
+
+enableValidation(config);
