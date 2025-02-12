c28ef2fbea2ee5abff5ea07a5ded27134c8db57f
Pierrebrunel <pierrebruneljoseph13@gmail.com>
2025-02-12 15:54:17 -0500

    check in

--- scripts/formvalidator.js
+++ scripts/formvalidator.js
@@ -1,3 +1,26 @@
-class  FormValidator {
-    
+class FormValidator {
+  constructor(settings, formElement) {
+    this._inputSelector = settings.inputSelector;
+    this._submitButtonSelector = settings.submitButtonSelector;
+    this._inactiveButtonClass = settings.inactiveButtonClass;
+    this._inputErrorClass = settings.inputErrorClass;
+    this._errorClass = settings.errorClass;
+
+    this._formElement = formElement;
+  }
+
+  enableValidation() {}
 }
+
+const editFormValidator = new FormValidator(settings, editForm);
+editFormValidator.enableValidation();
+// const addFormValidator = new FormValidator(settings, addForm);
+
+// const settings = {
+//   formSelector: ".modal__form",
+//   inputSelector: ".modal__input",
+//   submitButtonSelector: ".modal__button",
+//   inactiveButtonClass: "modal__button_disabled",
+//   inputErrorClass: "modal__input_type_error",
+//   errorClass: "modal__error_visible",
+// };
