d54d0860d712f7866b7b9d048a9189a88403eaa7
Pierrebrunel <pierrebruneljoseph13@gmail.com>
2025-02-24 03:01:29 -0500

    fix pages and the incorrect code

--- scripts/ForrmValidator.js
+++ scripts/ForrmValidator.js
@@ -7,7 +7,7 @@ class FormValidator {
     this._errorClass = settings.errorClass;
 
     this._form = formEl;
-    this._inputList = [...this._form.querySelectorAll("input")];
+    this._inputList = [...this._form.querySelectorAll(this._inputSelector)];
     this._submitButton = this._form.querySelector(this._submitButtonSelector);
   }
 
