1d5ebf3a71a7cd1d5bc08c8c29eb23c4a04c8abc
Pierrebrunel <pierrebruneljoseph13@gmail.com>
2025-04-09 17:08:19 -0400

    update refactoring and add the userinfo

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,71 +3,85 @@ import Card from "../scripts/Card.js";
 import FormValidator from "../scripts/FormValidator.js";
 import PopupWithForm from "../scripts/PopupWithForm.js";
 import PopupWithImage from "../scripts/PopupWithImage.js";
+import Section from "../scripts/Section.js";
+import UserInfo from "../scripts/UserInfo.js";
 import { initialCards, validationSettings } from "../utils/constants.js";
 
-/* DOM Elements */
+// DOM
 const profileEditButton = document.querySelector("#profile__edit-button");
-const profileTitle = document.querySelector("#profile__title");
-const profileDescription = document.querySelector("#profile__description");
+const profileAddButton = document.querySelector(".profile__add-button");
 const profileTitleInput = document.querySelector("#profile-title-input");
 const profileDescriptionInput = document.querySelector("#profile-description-input");
-const profileAddButton = document.querySelector(".profile__add-button");
-const cardListEl = document.querySelector(".cards__list");
 const profileEditForm = document.querySelector("#profile-edit-modal .modal__form");
 const profileAddForm = document.querySelector("#profile-add-modal .modal__form");
 
-/* Validators */
-const editFormValidator = new FormValidator(validationSettings, profileEditForm);
-editFormValidator.enableValidation();
+// User Info
+const userInfo = new UserInfo({
+  nameSelector: "#profile__title",
+  jobSelector: "#profile__description",
+});
 
-const addFormValidator = new FormValidator(validationSettings, profileAddForm);
-addFormValidator.enableValidation();
-
-/* Popups */
+// Popups
 const imagePopup = new PopupWithImage("#image__open-modal");
 imagePopup.setEventListeners();
 
 const profileEditPopup = new PopupWithForm("#profile-edit-modal", (formData) => {
-  profileTitle.textContent = formData["profile-title-input"];
-  profileDescription.textContent = formData["profile-description-input"];
+  userInfo.setUserInfo({
+    name: formData["profile-title-input"],
+    job: formData["profile-description-input"]
+  });
   profileEditPopup.close();
 });
 profileEditPopup.setEventListeners();
 
 const profileAddPopup = new PopupWithForm("#profile-add-modal", (formData) => {
-  const name = formData["modal__input_type_title"];
-  const link = formData["modal__input_type_url"];
-  renderCard({ name, link });
+  const cardData = {
+    name: formData["modal__input_type_title"],
+    link: formData["modal__input_type_url"]
+  };
+  const cardElement = createCard(cardData);
+  cardSection.addItem(cardElement);
   profileAddPopup.close();
   profileAddForm.reset();
   addFormValidator.toggleButtonState();
 });
 profileAddPopup.setEventListeners();
 
-/* Functions */
-function fillProfileForm() {
-  profileTitleInput.value = profileTitle.textContent;
-  profileDescriptionInput.value = profileDescription.textContent;
+// Validation
+const editFormValidator = new FormValidator(validationSettings, profileEditForm);
+editFormValidator.enableValidation();
+
+const addFormValidator = new FormValidator(validationSettings, profileAddForm);
+addFormValidator.enableValidation();
+
+// Card creation
+function createCard(data) {
+  const card = new Card(data, "#card-template", (cardData) => imagePopup.open(cardData));
+  return card.getView();
 }
 
-function handleCardClick({ name, link }) {
-  imagePopup.open({ name, link });
-}
+// Section for cards
+const cardSection = new Section(
+  {
+    items: initialCards,
+    renderer: (cardData) => {
+      const cardElement = createCard(cardData);
+      cardSection.addItem(cardElement);
+    }
+  },
+  ".cards__list"
+);
 
-function renderCard(cardData) {
-  const card = new Card(cardData, "#card-template", handleCardClick);
-  cardListEl.prepend(card.getView());
-}
+cardSection.renderItems();
 
-/* Event Listeners */
+// Event Listeners
 profileEditButton.addEventListener("click", () => {
-  fillProfileForm();
+  const { name, job } = userInfo.getUserInfo();
+  profileTitleInput.value = name;
+  profileDescriptionInput.value = job;
   profileEditPopup.open();
 });
 
 profileAddButton.addEventListener("click", () => {
   profileAddPopup.open();
 });
-
-/* Initial Cards */
-initialCards.forEach(renderCard);
