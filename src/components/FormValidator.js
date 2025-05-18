75d1e73db9f6c03b933ab355bc323b012ad12840
Pierrebrunel <pierrebruneljoseph13@gmail.com>
2025-05-18 12:39:19 -0400

    checking to

--- src/components/FormValidator.js
+++ src/components/FormValidator.js
@@ -68,3 +68,4 @@ class FormValidator {
 }
 
 export default FormValidator;
+
---
+++ src/components/PopupWithConfirm.js
@@ -0,0 +1,11 @@
+import Popup from "./Popup";
+export default class PopupWithConfirm extends Popup {
+
+  setEventListeners() {
+    super.setEventListeners();
+    // this._formElement.addEventListener("submit", (evt) => {
+    //   evt.preventDefault();
+    // });
+  }
+}
+
